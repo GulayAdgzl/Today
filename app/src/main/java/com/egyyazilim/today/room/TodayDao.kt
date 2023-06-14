8d5320c973247866ae20ae74a97bfe636e531a75
BetulCaglar <glyadgzl@hotmail.com>
2023-06-14 15:38:41 +0300

    Dao Coroutines suspend add

--- app/src/main/java/com/egyyazilim/today/room/TodayDao.kt
+++ app/src/main/java/com/egyyazilim/today/room/TodayDao.kt
@@ -6,15 +6,15 @@ import java.util.*
 @Dao
 interface TodayDao {
     @Query("SELECT * FROM today WHERE timestamp BETWEEN:startDate AND :endDate")
-    fun fetchBy(startDate: Date, endDate: Date): List<TodayEntity>
+    suspend fun fetchBy(startDate: Date, endDate: Date): List<TodayEntity>
     @Insert
-    fun todayEkle(today:TodayEntity)
+    suspend fun todayEkle(today:TodayEntity)
 
     @Update
-    fun todayGuncelle(today: TodayEntity)
+    suspend fun todayGuncelle(today: TodayEntity)
 
     @Delete
-    fun todaySil(today: TodayEntity)
+    suspend fun todaySil(today: TodayEntity)
 
     @Query("SELECT * FROM today_app")
     suspend fun tumToday():List<TodayEntity>
